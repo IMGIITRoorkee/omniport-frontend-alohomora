f4cafa11e1d39381c8ead4a0bdaf8a357b3be393
Praduman Goyal <goyal.praduman611@gmail.com>
2019-01-11 20:34:09 +0530

    Create app

---
+++ src/reducers/exampleReducer.js
@@ -0,0 +1,10 @@
+const initialState = {}
+
+const exampleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default exampleReducer
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import exampleReducer from './exampleReducer'
+
+const rootReducers = combineReducers({
+  exampleReducer
+})
+
+export default rootReducers
